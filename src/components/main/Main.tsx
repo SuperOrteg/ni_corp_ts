9646ad2cb8d687e7eda97372791000c38583d4c9
SuperOrteg <asseorteg@gmail.com>
2025-05-16 16:23:58 +0200

    Updated intro

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,11 +1,13 @@
+import type { FunctionComponent } from "react";
 import { MainText } from "./Main.styled";
 
-const Main = () => {
+const Main: FunctionComponent = () => {
   return (
     <MainText>
-      We advise and support teams and organizations throughout the transition to
-      where they must be tomorrow. Share knowledge, tools and methods to help
-      new social and economic actors by unlocking rapid and sustainable growth.
+      We design lean internal platforms that replace Excel chaos and scattered
+      tools. Built with no-code, low-code or lightweight dev, our systems are
+      fast to deploy and easy to evolve. For ambitious teams who want to move
+      faster, with less effort.
     </MainText>
   );
 };
